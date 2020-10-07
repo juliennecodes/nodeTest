0dd52aeff83ee554a58a56036dbad47934381ddc
julienne <julienne.codes@gmail.com>
2020-10-07 06:55:31 -0400

    cleaned up code

--- gifts.js
+++ gifts.js
@@ -13,12 +13,8 @@ let strawberryKit = new Gift("Ecocube Strawberry Grow Kit", 10);
 let magnets = new Gift("Scrabble Magnet Set", 12);
 let notebook = new Gift("Leuchtturm Bullet Journal A5", 24);
 let wallet = new Gift("Love and Lore Wallet", 18);
-
+// -----------------------------------------------------------------------------
 let inventory = new Map();
-// inventory.set(lipbalm.name, lipbalm);
-
-//to get the object
-// inventory.get(lipbalm.name);
 
 function addToInventory(item){
   return inventory.set(item.name, item);
@@ -32,59 +28,45 @@ addToInventory(strawberryKit);
 addToInventory(magnets);
 addToInventory(notebook);
 addToInventory(wallet);
+// -----------------------------------------------------------------------------
+function withinPriceRange(item, price){
+  return  item.price <= price;
+}
 
-// function underFive(item){
-//   return item.price <= 5;
-// }
-
-function underFive([name, item]){
+function underFive([itemName, item]){
   return item.price <= 5;
-}
-
-function underTen(item){
-  return item.price <= 10;
-}
-
-function underFifteen(item){
-  return item.price <= 15;
-}
-
-function underTwentyFive(item){
-  return item.price <= 25;
 }
 
 function fiveDollarGifts(items){
   let gifts = new Map();
 
-  //doesn't work because filter is exclusively for array?
-  // return inventory.filter(item => underFive(item));
-
-  //doesn't work because map isn't iterable?
-  // for (let item of items){
-  //   if(underFive(item)){
-  //     return gifts.set(item.name, item);
-  //   }
-  // }
-
-
-  // for (const [key, value] of Object.entries(items)){
-  //   //why doesn't Object.entries work
-  //   console.log(`${key}`);
-  //   return gifts.set(key, value);
-  //   ERROR - returning inside a loop ends all the function not just the local loop iteration scope
-  // }
-
   for (const [key, value] of items){
-    //why does [key, value] work and not item - referring to item of items
     if(underFive([key, value])) {
-      gifts.set(key, value)
+      gifts.set(key, value);
     }
   }
 
   return gifts;
 }
 
-module.exports = {inventory, underFive, fiveDollarGifts};
-//ERROR LOGS
-//forgot to export the function and it resulted in the test module not having access to it
-//I typed module.export instead of module.exports
+module.exports = {coaster, resistanceBands, inventory, addToInventory, withinPriceRange, underFive, fiveDollarGifts };
+
+//QUESTIONS
+//doesn't work because filter is exclusively for array?
+// return inventory.filter(item => underFive(item));
+
+//why does [key, value] work and not item - referring to item of items
+// for (let item of items){
+//   if(underFive(item)){
+//     gifts.set(item.name, item);
+//   }
+// }
+//VS
+// for (const [key, value] of items){
+//    if(underFive([key, value])) {
+//      gifts.set(key, value)
+//    }
+//  }
+
+//why doesn't Object.entries work on map objects
+//Object.entries(invetory) returns []
--- gifts.test.js
+++ gifts.test.js
@@ -1,55 +1,65 @@
 // const {expect, test} = require("@jest/globals");
-const{inventory, addToInventory, underFive, fiveDollarGifts } = require("./gifts.js");
+const{coaster, resistanceBands, inventory, addToInventory, withinPriceRange, underFive, fiveDollarGifts } = require("./gifts.js");
 
-// test("item is added to inventory", () =>{
-//   //given
-//   let lipbalm = inventory.get("Rosebud Salve in Tin");
-//
-//   //when
-//   addToInventory.set(lipbalm);
-//
-//   //then
-//   //how do you test for side effect functions?
-//   //check that the length increased?
-//   expect(addToInventory.set(lipbalm)).toBe
-//
-// });
-
-// test("item is under five dollars", () => {
-//   //given
-//   let resistanceBands = inventory.get("Three Level Resistance Bands");
-//
-//   //when
-//   underFive(resistanceBands);
-//
-//   //then
-//   expect(underFive(resistanceBands)).toBe(true);
-//
-// });
-//
-// test("item is over five dollars", () => {
-//   //given
-//   let lipbalm = inventory.get("Rosebud Salve in Tin");
-//
-//   //when
-//   underFive(lipbalm);
-//
-//   //then
-//   expect(underFive(lipbalm)).toBe(false);
-//
-// });
-
-test("get all items under five dollars", ()=> {
+test("item is under five dollars", () => {
   //given
-  //inventory
+  let resistanceBands = inventory.get("Three Level Resistance Bands");
 
   //when
-  fiveDollarGifts(inventory);
+  withinPriceRange(resistanceBands, 5);
 
   //then
-  //is this too specific? how do you test for implementation?
-  expect(fiveDollarGifts(inventory)).toBe({"Beer Mat Coaster Games": {name: "Beer Mat Coaster Games", price: 3} , "Three Level Resistance Bands": {name: "Three Level Resistance Bands", price: 5}});
-    // expect(fiveDollarGifts(inventory)).toBe({"Beer Mat Coaster Games" => {name: "Beer Mat Coaster Games", price: 3} , "Three Level Resistance Bands" => {name: "Three Level Resistance Bands", price: 5}});
-    //this was what was expected but it says parsing error?
+  expect(withinPriceRange(resistanceBands, 5)).toBe(true);
+});
+
+// -----------------------------------------------------------------------------
+test("item is over five dollars", () => {
+  //given
+  let lipbalm = inventory.get("Rosebud Salve in Tin");
+
+  //when
+  withinPriceRange(lipbalm, 5);
+
+  //then
+  expect(withinPriceRange(lipbalm, 5)).toBe(false);
+});
+// -----------------------------------------------------------------------------
+test("function will return an array that contains items under five dollars", () => {
+
+
+  fiveDollarGifts(inventory);
+
+  // expect(fiveDollarGifts(inventory)).toContain({"Beer Mat Coaster Games": coaster});
+  // // expect(fiveDollarGifts(inventory)).toContain({resistanceBands.name: resistanceBands});
+  // expect(fiveDollarGifts(inventory)).toContain({"Three Level Resistance Bands": resistanceBands});
+  // expect(fiveDollarGifts(inventory)).toContain({"Beer Mat Coaster Games": coaster}, {"Three Level Resistance Bands": resistanceBands});
+  //how come this only shows the coaster as the expected when I have the resistanceBands specified as well?
+
+  expect(fiveDollarGifts(inventory)).toContain({"Beer Mat Coaster Games": coaster}, {"Three Level Resistance Bands": resistanceBands});
 
 })
+// -----------------------------------------------------------------------------
+
+//QUESTIONS
+//how do you test for side effect functions?
+//check that the length increased?
+
+//is this too specific? how do you test for implementation in this case?
+// expect(fiveDollarGifts(inventory)).toBe({"Beer Mat Coaster Games": {name: "Beer Mat Coaster Games", price: 3} , "Three Level Resistance Bands": {name: "Three Level Resistance Bands", price: 5}});
+
+// expect(fiveDollarGifts(inventory)).toBe({"Beer Mat Coaster Games" => {name: "Beer Mat Coaster Games", price: 3} , "Three Level Resistance Bands" => {name: "Three Level Resistance Bands", price: 5}});
+//how do I represent a map object, even if the result and expect match up, it shows an error because : shows up as =>
+//when I type => instead, it says parsing error
+
+//you can't do property names in toBe...?
+// expect(fiveDollarGifts(inventory)).toContain({resistanceBands.name: resistanceBands}); //returns parsing error
+
+//what are the terms?
+//expect(x).y(z), are there official terms for x,y,z
+
+//you can only do one toBe checks? or can you do multiples and I just don't know how?
+
+// expect(fiveDollarGifts(inventory)).toContain({"Beer Mat Coaster Games": coaster}, {"Three Level Resistance Bands": resistanceBands});
+//how come this only shows the coaster as the expected when I have the resistanceBands specified as well?
+//because they have to be in a group?
+//I put them in an array for the resistanceBands to show up but a map is not an array? so it creates an error
